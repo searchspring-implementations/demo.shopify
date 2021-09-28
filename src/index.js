5b054072bb6a346fd8f08f1c24dc9137713f5f77
kevin <kevin@searchspring.com>
2021-09-28 15:58:08 -0600

    changing to boolean for prefetch

--- src/index.js
+++ src/index.js
@@ -95,7 +95,7 @@ const config = {
 						component: async () => {
 							return (await import('./components/Content')).Content;
 						},
-						prefetch: context.collection || context.searchPage,
+						prefetch: Boolean(context.collection || context.searchPage),
 						hideTarget: true,
 					},
 				],
